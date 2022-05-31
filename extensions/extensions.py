2ac081c9b8976d8b4b8294d33e343ddd0ba701b7
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-05-31 17:31:33 +0200

    Proplem_set1_File Extensions
    In a file called extensions.py, implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, case-insensitively, in any of these suffixes:

---
+++ extensions/extensions.py
@@ -0,0 +1,14 @@
+filename =input("File name: ").lower()
+
+if '.gif' in filename:
+    print("image/gif")
+elif '.png'in filename:
+    print("image/png")
+elif '.jpg'in filename:
+    print("image/jpeg")
+elif '.jpeg'in filename:
+    print("image/jpeg")
+elif '.pdf'in filename:
+    print("application/pdf")
+
+

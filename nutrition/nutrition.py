1e9717766360264371b12532fa9e0047a74e7573
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-06-01 13:31:32 +0200

    problem_set2_NUTRITION FACTS

---
+++ nutrition/nutrition.py
@@ -0,0 +1,21 @@
+
+fruits = {
+    "Apple": 130,
+    "Avocado": 50,
+    "Banana": 110,
+    "Cantaloupe": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lime": 20,
+    "Nectarine": 60,
+
+}
+
+fruit_asked = input("Item: ")
+
+for key in fruits:
+    if key in fruit_asked.lower():
+        print("Calories: ", fruits[key])
